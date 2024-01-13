75e0e29157732d7276f6eefb86680c3a72376ce3
Rian <rgillard@nuccimedical.com>
2024-01-13 17:11:54 -0500

    Add files via upload

---
+++ stack.js
@@ -0,0 +1,62 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to the top of the stack. Returns undefined. */
+  push(val) {
+    const newNode = new Node(val);
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+    const removedValue = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    if (this.isEmpty()) {
+      this.last = null;
+    }
+    return removedValue;
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Stack;
